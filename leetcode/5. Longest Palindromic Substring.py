655648b3fe91e9311f7655167b0db742d28d5e9b
v_王超3 <v_wangchao3@xdf.cn>
2020-01-03 14:39:16 +0800

    expand from mid

--- leetcode/5. Longest Palindromic Substring.py
+++ leetcode/5. Longest Palindromic Substring.py
@@ -7,3 +7,31 @@ Input: "babad"
 Output: "bab"
 Note: "aba" is also a valid answer.
 """
+
+
+class Solution:
+    def is_palindrome(self, substring):
+        return substring == substring[::-1]
+
+    def longestPalindrome(self, s: str) -> str:
+        # $#b#a#b#, $#a#b#b#a#
+        result, longest_radius = '', 0
+        s = '$' + '#'.join(s) + '#'
+        # 以每个字符为假想中心，进行扩展判断是否是回文串
+        print(s)
+        for i, c in enumerate(s):
+            mid, radius = i, 1
+            while (mid - radius) >= 0 and (mid + radius) < len(s):
+                if self.is_palindrome(s[mid - radius:mid + radius + 1]):
+                    radius += 1
+                else:
+                    break
+            if radius > longest_radius:
+                longest_radius = radius
+                result = s[mid - radius + 1:mid + radius]
+        result = ''.join(result.split('#'))
+        return result
+
+
+s = Solution()
+s.longestPalindrome('abcd')
