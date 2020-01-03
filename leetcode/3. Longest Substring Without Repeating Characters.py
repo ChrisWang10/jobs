dd5b5c58c2b45a9b4c932eeb00a33d5ac9a6c4d3
v_王超3 <v_wangchao3@xdf.cn>
2020-01-03 11:05:37 +0800

    speed up

--- leetcode/3. Longest Substring Without Repeating Characters.py
+++ leetcode/3. Longest Substring Without Repeating Characters.py
@@ -15,6 +15,9 @@ Explanation: The answer is "b", with the length of 1.
 
 
 class Solution:
+    """
+    遍历字符串，
+    """
 
     def new(self, s: str) -> int:
         result = 0
@@ -23,7 +26,6 @@ class Solution:
         for i in range(0, len(s)):
             if s[i] in D:
                 maxResult = max(D[s[i]], maxResult)
-
             result = max(result, i - maxResult + 1)
             D[s[i]] = i + 1
 
@@ -38,20 +40,29 @@ class Solution:
             if start == end:
                 record[s[start]] = start
                 end += 1
-            else:
-                if s[end] not in record:
-                    record[s[end]] = end
-                    end += 1
-                    cur_length += 1
-                    if cur_length > max_length:
-                        max_length = cur_length
+            elif s[end] not in record:
+                record[s[end]] = end
+                end += 1
+                cur_length += 1
+                if cur_length > max_length:
+                    max_length = cur_length
                 # end指向的位置的值之前访问过
-                else:
-                    start = record[s[end]] + 1
-                    end = start
-                    record = {}
-                    cur_length = 1
+            else:
+                start = record[s[end]] + 1
+                end = start
+                record = {}
+                cur_length = 1
         return max_length
+
+    def review(self, s: str) -> int:
+        result, cur_max = 0, 0
+        record = dict()
+        for i, c in enumerate(s):
+            if c in record:
+                cur_max = max(cur_max, record[c])
+            result = max(result, i - cur_max + 1)
+            record[c] = i + 1
+        return result
 
 
 s = Solution()
