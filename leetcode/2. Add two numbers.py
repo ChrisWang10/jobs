dfa6b34a0b08b8dd41f5aa47a10c958d4e3c56a0
v_王超3 <v_wangchao3@xdf.cn>
2020-01-03 09:28:10 +0800

    link list

--- leetcode/2. Add two numbers.py
+++ leetcode/2. Add two numbers.py
@@ -8,3 +8,38 @@ Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
 Output: 7 -> 0 -> 8
 Explanation: 342 + 465 = 807.
 """
+
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+class Solution:
+    def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
+        ret = ListNode(0)
+        cur = ret
+        carry = 0
+        while l1 and l2:
+            carry, next_value = divmod(l1.val + l2.val + carry, 10)
+            cur.next = ListNode(next_value)
+            l1 = l1.next
+            l2 = l2.next
+            cur = cur.next
+        while l1:
+            carry, next_value = divmod(l1.val + carry, 10)
+            cur.next = ListNode(next_value)
+            l1 = l1.next
+            cur = cur.next
+        while l2:
+            carry, next_value = divmod(l2.val + carry, 10)
+            cur.next = ListNode(next_value)
+            l2 = l2.next
+            cur = cur.next
+        while carry:
+            cur.next = ListNode(carry)
+            carry = 0
+
+        return ret.next
