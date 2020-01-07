3eb393b55819042d091db583d1081841cf634969
ChrisWang10 <wchao118@gmail.com>
2020-01-07 13:16:11 +0800

    manacher

--- leetcode/5. Longest Palindromic Substring.py
+++ leetcode/5. Longest Palindromic Substring.py
@@ -17,7 +17,7 @@ class Solution:
         # $#b#a#b#, $#a#b#b#a#
         result, longest_radius = '', 0
         s = '$' + '#'.join(s) + '#'
-        # 以每个字符为假想中心，进行扩展判断是否是回文串
+        # Take each character as the imaginary center, and expand to determine whether it is a palindrome.
         print(s)
         for i, c in enumerate(s):
             mid, radius = i, 1
@@ -33,6 +33,35 @@ class Solution:
         return result
 
     def longestPalindrome_new(self, s: str) -> str:
+        """
+        The above method does not use the palindrome information already known
+        for example: a s d b d s k
+        at this moment we know that b is the center of 's d b d s',
+        then don't need to calculate d after b because we already know the radius of d before b according to symmetric
+        characteristic of  Palindrome
+        """
+
+        def longestPalindrome(self, s: str) -> str:
+            if len(s) <= 1:
+                return s
+            s = '$' + '#'.join(s) + '#'
+            maxRight = 0
+            maxCenter = 0
+            radius = [0] * len(s)
+            result = ''
+            for i in range(len(s)):
+                if len(s) - i <= maxRight - maxCenter:
+                    break
+                radius[i] = 1 if i >= maxRight else min(radius[2 * maxCenter - i], maxRight - i)
+
+                while i - radius[i] >= 0 and i + radius[i] < len(s) and s[i - radius[i]] == s[i + radius[i]]:
+                    radius[i] += 1
+
+                if radius[i] > maxRight - maxCenter:
+                    maxRight = radius[i] + i
+                    maxCenter = i
+                    result = s[i - radius[i] + 1:i + radius[i]]
+            return result.replace('#', '')
 
 
 s = Solution()
